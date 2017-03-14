22cb75078b16922fd369bdd8dfef2a1483f91f6c
BreakingBran <lancepereira99@gmail.com>
2017-03-14 12:50:01 -0400

    first attempt at merge sort

---
+++ Week 1/src/MergeSort.java
@@ -0,0 +1,23 @@
+
+/**
+ * MergeSort
+ * @author: Lance Pereira
+ * @course: ICS4U1
+ * @date: Mar 14, 2017
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class MergeSort {
+	public static void main(String args[]) {
+		// TO DO
+		// TO DO
+		// TO DO
+
+		Scanner sc = new Scanner(System.in);
+
+		int someVariable = sc.nextInt();
+		System.out.print("Hey Cutie");
+	}
+}
